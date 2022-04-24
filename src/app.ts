5a7261f945b17b4690a621d1b14fbc36b8b16ed5
Nina Lisitsinskaya <vvatashi@protonmail.com>
2022-04-24 13:09:51 +0600

    Add logger

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,5 @@
 import Koa from 'koa';
+import logger from 'koa-logger';
 import cors from '@koa/cors';
 import helmet from 'koa-helmet';
 import Router from 'koa-router';
@@ -14,6 +15,10 @@ export function createApp() {
   router.get('/sse', sseController.index);
 
   const app = new Koa();
+  if (process.env.NODE_ENV === 'development') {
+    app.use(logger());
+  }
+
   app.use(helmet.contentSecurityPolicy());
   app.use(helmet.referrerPolicy());
   app.use(helmet.noSniff());
